dffc65d975326f32f47e839af0404a02c7aa9986
Chris Ware <c.p.ware@hotmail.com>
2022-11-04 14:08:26 -0700

    layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Next Amazon</title>
-        <meta name='description' content='Amazon Clone' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <h1 className='text-3xl font-bold'>Hello!</h1>
+      <Layout title='Home Page'>Home Page</Layout>
     </div>
   );
 }
