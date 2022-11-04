7a5154d1bc62c486c3daad9a68ae3f3a570dd2d4
Chris Ware <c.p.ware@hotmail.com>
2022-11-04 12:48:54 -0700

    Styles update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1 className='text-3xl font-bold underline'>
-    Hello!
-   </h1>
-  )
+    <div>
+      <Head>
+        <title>Next Amazon</title>
+        <meta name='description' content='Amazon Clone' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1 className='text-3xl font-bold'>Hello!</h1>
+    </div>
+  );
 }
