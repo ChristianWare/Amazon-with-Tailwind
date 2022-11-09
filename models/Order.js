1e2d053e94128349b0becf894ff458641708a919
Chris Ware <c.p.ware@hotmail.com>
2022-11-09 16:44:12 -0700

    Create Order Screen

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    orderitems: [
+    orderItems: [
       {
         name: { type: String, required: true },
         quantity: { type: Number, required: true },
