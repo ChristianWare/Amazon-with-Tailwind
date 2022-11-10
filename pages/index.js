88be7236ab88a85b2a21414076e7cb15eba5325a
Chris Ware <c.p.ware@hotmail.com>
2022-11-10 12:50:00 -0700

    Added Key prop to map

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,7 @@ export default function Home({ products }) {
         <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
           {products.map((product) => (
             <ProductItem
+              key={product.slug}
               product={product}
               addToCartHandler={addToCartHandler}
             />
