c59ab96859a40f643c444f3795a7f13f2816b3e0
Chris Ware <c.p.ware@hotmail.com>
2022-11-10 22:10:00 -0700

    Authorized Route for delivered API

--- pages/api/admin/orders/[id]/deliver.js
+++ pages/api/admin/orders/[id]/deliver.js
@@ -1,7 +1,13 @@
+import { getSession } from "next-auth/react";
 import Order from "../../../../../models/Order";
 import db from "../../../../../utils/db";
 
 const handler = async (req, res) => {
+  const session = await getSession({ req });
+  if (!session || (session && !session.user.isAdmin)) {
+    return res.status(401).send("Error: Signin Required");
+  }
+
   await db.connect();
   const order = await Order.findById(req.query.id);
   if (order) {
